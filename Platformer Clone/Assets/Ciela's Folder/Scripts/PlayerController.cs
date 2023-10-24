64513f2661a624d3741d8b80623a7ebd5ca429b2
cielaparent <148916741+cielaparent@users.noreply.github.com>
2023-10-24 16:58:41 -0700

    Created a bullet script

--- Platformer Clone/Assets/Ciela's Folder/Scripts/PlayerController.cs
+++ Platformer Clone/Assets/Ciela's Folder/Scripts/PlayerController.cs
@@ -1,16 +1,9 @@
-using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// Parent, Ciela
-// 10/23/23
-// Controls players XP
-
-
-public class PlayerHealth : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    public void 
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +13,6 @@ public class PlayerHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         
+        
     }
 }
--- Platformer Clone/Assets/Ciela's Folder/Scripts/RegularBullet.cs
+++ Platformer Clone/Assets/Ciela's Folder/Scripts/RegularBullet.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Unity.VisualScripting;
 using UnityEngine;
+
+// Parent, Ciela
+// 10/23/23
+// Handles when bullet is fired and how much XP is taken
 
 public class RegularBullet : MonoBehaviour
 {
+    public GameObject bulletPrefab;
+    public float spawnrate = 1f;
+    public bool shootRight = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("ShootBullet", 0, spawnrate);
     }
+    /// <summary>
+    /// shoots a bullet and sets whih direction it should go
+    /// </summary>
 
-    // Update is called once per frame
-    void Update()
+    private void ShootBullet() 
     {
-        
+        GameObject laserInstance = Instantiate(bulletPrefab, transform.position, bulletPrefab.transform.rotation);
+        laserInstance.GetComponent<Bullet>().goingRight = shootRight;
     }
+    if //bullet hits player, 1 XP is taken away
+    if // space bar "fire button" is pressed, a laser is released
+    if // space bar is held, laser shoots at fire rate
+    if // space bar is released, player stops shooting
 }
