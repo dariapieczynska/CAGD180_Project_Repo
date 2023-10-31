a84bfad556178131de159ae2f06d0de93fecb22d
dariapieczynska <148916267+dariapieczynska@users.noreply.github.com>
2023-10-31 14:48:41 -0700

    heavy bullet things and coroutines for shooting

--- Platformer Clone/Assets/Daria's Folder/Daria's Scripts/Gun.cs
+++ Platformer Clone/Assets/Daria's Folder/Daria's Scripts/Gun.cs
@@ -7,6 +7,7 @@ public class Gun : MonoBehaviour
     public GameObject regularBullet;
     public GameObject heavyBullet;
     public bool canShoot=true;
+    public bool heavyBulletObject = false; 
 
   
     // Start is called before the first frame update
@@ -26,10 +27,34 @@ public class Gun : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Return))
             {
-                //shooting a bullet
-                Instantiate(regularBullet, transform.position, transform.rotation);
+                if (heavyBulletObject)
+                {
+                    //shooting a bullet
+                    Instantiate(heavyBullet, transform.position, transform.rotation);
+                    StartCoroutine(CoolDown());
+                }
+                else
+                {
+                    Instantiate(regularBullet, transform.position, transform.rotation);
+                    StartCoroutine(CoolDown());
+                }
             }
 
         }
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="heavy_bullet")
+        {
+            Debug.Log("hit heavy bullet");
+            heavyBulletObject=true;
+            other.gameObject.SetActive(false);
+        }
+    }
+    public IEnumerator CoolDown()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(.5f);
+        canShoot = true; 
     }
 }
